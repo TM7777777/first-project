d95fddcee353d8d08986460ce9b42e797cd62b95
TM7777777 <danbogameden@gmail.com>
2021-07-03 19:03:05 +0300

    changed html&js code, added style.css

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-const button = document.querySelector('button');
+const button = document.querySelector('.addBtn');
 const input = document.querySelector('input');
 const list = document.querySelector('ul');
 
 let arr = ['buy bread', 'go for a walk', 'to do smth', 'drink water'];
 
 arr.map((el, ind) => {
-  list.innerHTML += `<li class='numb ${ind + 1}'>${el}<button class="del ${
-    ind + 1
-  }">del</button></li>`;
-  const deleteBtns = document.querySelectorAll('.del');
-  var deleteBtnsArr = Array.from(deleteBtns);
-  deleteBtnsArr.map((el, id) => {
-    el.addEventListener('click', function () {
-      const liItem = document.getElementsByClassName(`${id + 1}`)[0];
-      liItem.remove();
-    });
-  });
+  list.innerHTML += `<li class='numb ${ind}'><span><b>${
+    ind + 1 + ')'
+  }</b> ${el}</span><button class="del ${ind}">X</button></li>`;
+  delBtn();
 });
 
-let ind = arr.length + 1;
-
 button.addEventListener('click', function () {
-  list.innerHTML += `<li class='numb ${ind}'>${input.value}<button class="del ${ind}">del</button></li>`;
-  ind = ++ind;
+  if (input.value === '') {
+    alert('write smth');
+  } else {
+    const ind = arr.length;
+    list.innerHTML += `<li class='numb ${ind}'><span><b>${ind + 1 + ')'}</b> ${
+      input.value
+    }</span><button class="del ${ind}">X</button></li>`;
+    arr.push(input.value);
+    delBtn();
+    if (arr.length > 0) {
+      document.getElementsByClassName('ul')[0].style.borderStyle = 'solid';
+    }
+    console.log(arr);
+  }
+});
+
+function delBtn() {
   const delBtns = document.querySelectorAll('.del');
   var delBtnsArr = Array.from(delBtns);
   delBtnsArr.map((el) => {
     el.addEventListener('click', function (e) {
       const ind = +e.target.className.replace('del', '');
-      const liItem = document.getElementsByClassName(`${ind}`)[0];
-      liItem.remove();
+      list.innerHTML = '';
+      arr = arr.filter((el, id) => id !== ind);
+      arr.map((el, ind) => {
+        list.innerHTML += `<li class='numb ${ind}'><span><b>${
+          ind + 1 + ')'
+        }</b> ${el}</span><button class="del ${ind}">X</button></li>`;
+        delBtn();
+      });
+      if (arr.length < 1) {
+        document.getElementsByClassName('ul')[0].style.borderStyle = 'none';
+      }
+      console.log(arr);
     });
   });
-});
+}
